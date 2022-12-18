50f62833de757ec6cbed46691ed4afe3881b6231
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2022-12-18 16:42:32 +0530

    Create a campaign code done.

---
+++ client/src/context/index.jsx
@@ -0,0 +1,56 @@
+import React, { useContext, createContext } from "react";
+
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import { ethers } from "ethers";
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    "0xA0CE4Cd45Ec12F25cDafb3248113A89aa7C3aD62"
+  );
+
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign([
+        address,
+        form.title,
+        form.description,
+        form.target,
+        new Date(form.deadline).getTime(),
+        form.image,
+      ]);
+      console.log("Contract call success", data);
+    } catch (error) {
+      console.log("Contract call failure", error);
+    }
+  };
+
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
